8887f386fffc1193f8067db3fdb94e0269faaa07
jeffrafter <jeffrafter@github.com>
2020-04-05 22:18:03 -0700

    Try embedding the tufte files directly

---
+++ src/remark-figure-transformer.js
@@ -0,0 +1,37 @@
+import visit from "unist-util-visit";
+import HTML from "html-parse-stringify";
+
+function tufteFigureTransformer() {
+  return transformer;
+}
+
+export function transformer(tree) {
+  visit(tree, "tufteFigure", (node, index, parent) => {
+    const openTagAst = HTML.parse(node.attributes.openingHtml);
+
+    let figureProperties;
+
+    if (openTagAst) {
+      if (openTagAst[0].attrs.class) {
+        figureProperties = {
+          className: openTagAst[0].attrs.class,
+        };
+      }
+    }
+
+    const replacement = {
+      type: "paragraph",
+      children: node.children,
+      data: {
+        hName: "figure",
+        hProperties: figureProperties,
+      },
+    };
+    parent.children.splice(index, 1, replacement);
+  });
+
+  // only for test
+  return tree;
+}
+
+export default tufteFigureTransformer;
---
+++ src/remark-sidenotes-transformer.js
@@ -0,0 +1,97 @@
+import getSlug from "speakingurl";
+import visit from "unist-util-visit";
+import select from "unist-util-select";
+import toHAST from "mdast-util-to-hast";
+import toHTML from "hast-util-to-html";
+
+const MARGINNOTE_SYMBOL = "{-}";
+
+function sidenotes() {
+  return transformer;
+}
+
+const generateInputId = (isMarginNote, title) =>
+  `${isMarginNote ? "md" : "sd"}-${getSlug(title, { truncate: 20 })}`;
+
+const getReplacement = ({ isMarginNote, noteHTML }) => {
+  const inputId = generateInputId(isMarginNote, noteHTML);
+  const labelCls = `margin-toggle ${isMarginNote ? "" : "sidenote-number"}`;
+  const labelSymbol = isMarginNote ? "&#8853;" : "";
+  const noteTypeCls = isMarginNote ? "marginnote" : "sidenote";
+
+  return [
+    {
+      type: "html",
+      value: `<label for="${inputId}" class="${labelCls}">${labelSymbol}</label>`,
+    },
+    {
+      type: "html",
+      value: `<input type="checkbox" id="${inputId}" class="margin-toggle" />`,
+    },
+    {
+      type: "html",
+      value: `<span class="${noteTypeCls}">`,
+    },
+    {
+      type: "html",
+      value: noteHTML,
+    },
+    {
+      type: "html",
+      value: "</span>",
+    },
+  ];
+};
+
+const coerceToHtml = (nodeArray) =>
+  nodeArray.map((node) => toHTML(toHAST(node))).join("") || "";
+
+const extractNoteFromHtml = (note) => {
+  const matches = note.match(/(\s+)*({-})*\s*((.|\n)+)/);
+
+  return {
+    isMarginNote: matches[2] === MARGINNOTE_SYMBOL,
+    noteHTML: matches[3],
+  };
+};
+
+export function transformer(tree) {
+  // "Regular" Sidenotes/Marginnotes consisting of a reference and a definition
+  // Syntax for Sidenotes [^<number>] and somewhere else [^<number]: <markdown>
+  // Syntax for Marginnotes [^<descriptor>] and somewhere else [^<descriptor]: {-}
+  visit(tree, "footnoteReference", (node, index, parent) => {
+    const target = select(
+      tree,
+      `footnoteDefinition[identifier=${node.identifier}]`
+    );
+
+    if (!target.length) throw new Error("No coresponding note found");
+
+    const notesAst =
+      target[0].children.length && target[0].children[0].type === "paragraph"
+        ? target[0].children[0].children
+        : target[0].children;
+
+    const nodeDetail = extractNoteFromHtml(coerceToHtml(notesAst));
+
+    parent.children.splice(index, 1, ...getReplacement(nodeDetail));
+  });
+
+  visit(tree, "footnoteDefinition", (node, index, parent) => {
+    parent.children.splice(index, 1);
+  });
+
+  // "Inline" Sidenotes which do not have two parts
+  // Syntax: [^{-} <markdown>]
+  visit(tree, "footnote", (node, index, parent) => {
+    const notesAst = node.children;
+    const nodeDetail = extractNoteFromHtml(coerceToHtml(notesAst));
+
+    parent.children.splice(index, 1, ...getReplacement(nodeDetail));
+  });
+
+  // Only for testing
+  return tree;
+}
+
+export default sidenotes;
